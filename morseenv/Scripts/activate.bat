575d4996d3d9ae7175924b79e98885125fbb8e52
kavitharavichandran3805 <kavitharavichandran3805@gmail.com>
2025-03-25 22:34:31 +0530

    Updated local changes

--- morseenv/Scripts/activate.bat
+++ morseenv/Scripts/activate.bat
@@ -8,7 +8,11 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
+<<<<<<< HEAD
 set "VIRTUAL_ENV=D:\react-pro\morse code\morseenv"
+=======
+set VIRTUAL_ENV=C:\Users\Dell\Desktop\DESKTOP_FOLDER\MORSE CODE\morseenv
+>>>>>>> b18cd887 (Updated local changes)
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -24,8 +28,13 @@ set PYTHONHOME=
 if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
+<<<<<<< HEAD
 set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
 set "VIRTUAL_ENV_PROMPT=(morseenv) "
+=======
+set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+set VIRTUAL_ENV_PROMPT=(morseenv) 
+>>>>>>> b18cd887 (Updated local changes)
 
 :END
 if defined _OLD_CODEPAGE (
